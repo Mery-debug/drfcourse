1f8e6cd7b1601890c58a26f79782aa350abd8f29
Мария Шапошникова <m.a.shap58000@gmail.com>
2025-05-25 02:45:08 +0300

    user model

--- drfcours/users/models.py
+++ drfcours/users/models.py
@@ -1,5 +1,17 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
-    pass
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Адрес почты")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return {self.email}
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
